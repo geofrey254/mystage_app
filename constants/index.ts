5832d2cd28a2603b20c5d38343113702c7a66f4a
geofrey <devongeoffreymaina@gmail.com>
2024-10-03 08:12:10 +0300

    Added more bus stations

--- constants/index.ts
+++ constants/index.ts
@@ -3,14 +3,16 @@ export const busStages = [
   {
     id: 1,
     name: "Super Metro",
-    description: "Kikuyu, Kinoo, Regen, Muthiga, Uthiru, Co-operative",
+    description:
+      "Westlands, Chiromo, Kikuyu, Kinoo, Regen, Muthiga, Uthiru, Co-operative",
     latitude: -1.2840101533082702,
     longitude: 36.82549026121212,
   },
   {
     id: 2,
     name: "Enabled Sacco",
-    description: "Kikuyu, Kinoo, Muthiga, Uthiru, Co-operative, Regen",
+    description:
+      "Westlands, Chiromo, Kikuyu, Kinoo, Muthiga, Uthiru, Co-operative, Regen",
     latitude: -1.2852487667671404,
     longitude: 36.8262655823311,
   },
@@ -44,5 +46,39 @@ export const busStages = [
     description: "Eastleigh, Ngara",
     latitude: -1.2840904106799633,
     longitude: 36.82553720354892,
+  },
+
+  {
+    id: 7,
+    name: "Super Metro",
+    description:
+      "Westlands, Chiromo, Kikuyu, Kinoo, Muthiga, Uthiru, Co-operative, Regen",
+    latitude: -1.2844595345281486,
+    longitude: 36.82553720354892,
+  },
+
+  {
+    id: 8,
+    name: "NNK",
+    description: "Juja, Ruiru, Thika, Roysambu",
+    latitude: -1.2849938962100533,
+    longitude: 36.826140894310775,
+  },
+
+  {
+    id: 9,
+    name: "City Shuttle",
+    description:
+      "Westlands, Chiromo, Kikuyu, Kinoo, Muthiga, Uthiru, Co-operative, Regen",
+    latitude: -1.283722750498904,
+    longitude: 36.826140894310775,
+  },
+
+  {
+    id: 10,
+    name: "Enabled Sacco",
+    description: "Ngong, Rongai",
+    latitude: -1.2853378123841868,
+    longitude: 36.82632107879962,
   },
 ];
