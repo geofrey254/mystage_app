6d3f913de544671f1ba609a70e15977c3804f754
geofrey <devongeoffreymaina@gmail.com>
2024-10-04 14:46:35 +0300

    Made footer responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,10 +6,10 @@ function Footer() {
   return (
     <footer className="flex justify-center items-center p-2">
       <div className="container flex justify-center">
-        <div className="row flex text-center items-center gap-6">
+        <div className="row flex text-center items-center gap-2 md:gap-6 text-sm md:text-md">
           <div>
             <h4 className="text-sm text-[#ffa800] font-semibold">
-              &copy;{myDate.getFullYear()} &nbsp; <Link href="/">MyStage</Link>
+              &copy;{myDate.getFullYear()} <Link href="/">MyStage</Link>
             </h4>
           </div>
           <div className="text-[#392a0b]">|</div>
