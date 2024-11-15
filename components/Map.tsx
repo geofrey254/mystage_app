fa84ff081b051c7890411edec9cbe20e2d33f5eb
geofrey <devongeoffreymaina@gmail.com>
2024-11-15 13:31:54 +0300

    Adjusted the zoom level

--- components/Map.tsx
+++ components/Map.tsx
@@ -219,7 +219,7 @@ const Map = () => {
             <GoogleMap
               mapContainerStyle={containerStyle}
               center={currentLocation || center}
-              zoom={16}
+              zoom={14}
               options={{ styles: mapStyle, mapTypeId: "hybrid" }}
               onLoad={onMapLoad} // onLoad to initialize clustering
             >
