8571fbb6d059e9c660ba4642b570f45894a3047a
geofrey <devongeoffreymaina@gmail.com>
2024-11-15 17:01:27 +0300

    Working well

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,14 +1,13 @@
 "use client";
-
 import React, { useState, useEffect, useRef } from "react";
 import {
-  useLoadScript,
   GoogleMap,
+  LoadScript,
   Marker,
   InfoWindow,
 } from "@react-google-maps/api";
-import { MarkerClusterer } from "@googlemaps/markerclusterer";
-import { busStages } from "@/constants"; // Ensure busStages is an array of bus stages
+import { MarkerClusterer } from "@googlemaps/markerclusterer"; // Correctly import MarkerClusterer
+import { busStages } from "@/constants"; // Ensure busStages has an array of bus stages
 import mapStyle from "./mapStyle"; // Your custom map styles
 import { MapPin } from "lucide-react"; // Import the desired Lucide icon
 
@@ -39,12 +38,11 @@ const Map = () => {
     lng: number;
   } | null>(null);
 
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
-  });
+  const googleMapsKey: string =
+    process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "";
 
   const mapRef = useRef<google.maps.Map | null>(null);
-  const clustererRef = useRef<MarkerClusterer | null>(null);
+  const clustererRef = useRef<MarkerClusterer | null>(null); // Ref for MarkerClusterer
 
   const haversineDistance = (coords1, coords2) => {
     const toRad = (value) => (value * Math.PI) / 180;
@@ -86,7 +84,7 @@ const Map = () => {
         switch (error.code) {
           case error.PERMISSION_DENIED:
             alert(
-              "Permission denied. Please allow location access in your browser settings."
+              "Permission denied. Please allow location access in your browser settings"
             );
             break;
           case error.POSITION_UNAVAILABLE:
@@ -125,16 +123,20 @@ const Map = () => {
         return currDistance < prevDistance ? curr : prev;
       });
 
-      if (nearestStage && mapRef.current) {
-        mapRef.current.setCenter({
-          lat: nearestStage.latitude,
-          lng: nearestStage.longitude,
-        });
-        mapRef.current.setZoom(18);
+      if (nearestStage) {
+        if (mapRef.current) {
+          mapRef.current.setCenter({
+            lat: nearestStage.latitude,
+            lng: nearestStage.longitude,
+          });
+          mapRef.current.setZoom(18);
+        }
       }
-    } else if (currentLocation && mapRef.current) {
-      mapRef.current.setCenter(currentLocation);
-      mapRef.current.setZoom(16);
+    } else if (currentLocation) {
+      if (mapRef.current) {
+        mapRef.current.setCenter(currentLocation);
+        mapRef.current.setZoom(16);
+      }
     } else {
       setSelectedStage(null);
     }
@@ -174,79 +176,101 @@ const Map = () => {
       return marker;
     });
 
-    clustererRef.current = new MarkerClusterer({ map, markers });
+    clustererRef.current = new MarkerClusterer({
+      map,
+      markers,
+    });
   };
 
-  if (loadError) return <div>Error loading Google Maps</div>;
-  if (!isLoaded) return <div>Loading...</div>;
+  useEffect(() => {
+    if (mapRef.current && clustererRef.current) {
+      clustererRef.current.clearMarkers(); // Clear existing markers
+      const markers = filteredStages.map((stage) => {
+        const marker = new google.maps.Marker({
+          position: { lat: stage.latitude, lng: stage.longitude },
+          title: stage.name,
+        });
+
+        marker.addListener("click", () => {
+          setSelectedStage(stage);
+        });
+
+        return marker;
+      });
+
+      clustererRef.current.addMarkers(markers);
+    }
+  }, [filteredStages]);
 
   return (
     <section className="p-0 md:p-8 bg-[#462f01]">
       <div className="md:rounded-xl border-4 md:border-8 border-[#ffa800] shadow-xl shadow-[#ffa800]">
-        <div className="relative">
-          <div className="absolute w-full flex justify-center bottom-0 px-3 py-2 md:py-4 md:px-4">
-            <input
-              type="text"
-              placeholder="Where are you headed to?"
-              value={searchTerm}
-              onChange={handleSearchChange}
-              className="z-10 bg-[#0000006e] bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10 p-2 md:p-6 rounded-md ps-8 border-2 border-[#ffa800] focus:outline-none focus:ring-0 focus:border-transparent shadow-md shadow-[#3a2b0d] w-[50vh] md:w-[80vh] h-[7vh] md:h-[5vh] placeholder:text-white animate-bounce focus:animate-none text-white"
-            />
-          </div>
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={currentLocation || center}
-            zoom={currentLocation ? 16 : 14}
-            options={{
-              styles: mapStyle,
-              mapTypeId: "hybrid",
-              mapTypeControl: false,
-            }}
-            onLoad={onMapLoad}
-          >
-            {selectedStage && (
-              <InfoWindow
-                position={{
-                  lat: selectedStage.latitude,
-                  lng: selectedStage.longitude,
-                }}
-                onCloseClick={() => setSelectedStage(null)}
-                options={{
-                  pixelOffset: new window.google.maps.Size(0, -30),
-                  maxWidth: 2000,
-                }}
-              >
-                <div className="p-4 flex flex-col gap-4">
-                  <h4 className="font-bold text-lg text-center">
-                    {selectedStage.name}
-                  </h4>
-                  <div className="bg-[#ffaa0069] grid grid-cols-2 gap-y-2 md:grid-cols-3 md:gap-y-1 md:gap-x-2 p-4 text-black rounded-lg shadow-md">
-                    {selectedStage?.description
-                      .split(",")
-                      .map((stage, index) => (
-                        <p key={index} className="text-sm flex">
-                          <span className="hidden md:flex">{"📌"}</span>
-                          {stage.trim()}
-                        </p>
-                      ))}
-                  </div>
-                </div>
-              </InfoWindow>
-            )}
-
-            {currentLocation && (
-              <Marker
-                position={currentLocation}
-                icon={{
-                  url: "map-marker.png",
-                  scaledSize: new window.google.maps.Size(60, 60),
-                  origin: new window.google.maps.Point(0, 0),
-                }}
-                onClick={() => setSelectedStage(null)}
+        <LoadScript googleMapsApiKey={googleMapsKey}>
+          <div className="relative">
+            <div className="absolute w-full flex justify-center bottom-0 px-3 py-2 md:py-4 md:px-4">
+              <input
+                type="text"
+                placeholder="Where are you headed to?"
+                value={searchTerm}
+                onChange={handleSearchChange}
+                className="z-10 bg-[#0000006e] bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-10 p-2 md:p-6 rounded-md ps-8 border-2 border-[#ffa800] focus:outline-none focus:ring-0 focus:border-transparent shadow-md shadow-[#3a2b0d] w-[50vh] md:w-[80vh] h-[7vh] md:h-[5vh] placeholder:text-white animate-bounce focus:animate-none text-white"
               />
-            )}
-          </GoogleMap>
-        </div>
+            </div>
+            <GoogleMap
+              mapContainerStyle={containerStyle}
+              center={currentLocation || center}
+              zoom={currentLocation ? 16 : 14}
+              options={{
+                styles: mapStyle,
+                mapTypeId: "hybrid",
+                mapTypeControl: false,
+              }}
+              onLoad={onMapLoad} // onLoad to initialize clustering
+            >
+              {selectedStage && (
+                <InfoWindow
+                  position={{
+                    lat: selectedStage.latitude,
+                    lng: selectedStage.longitude,
+                  }}
+                  onCloseClick={() => setSelectedStage(null)}
+                  options={{
+                    pixelOffset: new window.google.maps.Size(0, -30),
+                    maxWidth: 2000,
+                  }}
+                >
+                  <div className="p-4 flex flex-col gap-4">
+                    <h4 className="font-bold text-lg text-center">
+                      {selectedStage.name}
+                    </h4>
+                    <div className="bg-[#ffaa0069] grid grid-cols-2 gap-y-2 md:grid-cols-3 md:gap-y-1 md:gap-x-2 p-4 text-black rounded-lg shadow-md">
+                      {selectedStage?.description
+                        .split(",")
+                        .map((stage, index) => (
+                          <p key={index} className="text-sm flex">
+                            <span className="hidden md:flex">{"📌"}</span>
+                            {stage.trim()}
+                          </p>
+                        ))}
+                    </div>
+                  </div>
+                </InfoWindow>
+              )}
+
+              {currentLocation && (
+                <Marker
+                  position={currentLocation}
+                  icon={{
+                    url: "map-marker.png", // Path to your custom marker icon
+                    scaledSize: new window.google.maps.Size(60, 60), // Adjust size if needed
+                    origin: new window.google.maps.Point(0, 0),
+                  }}
+                  onClick={() => setSelectedStage(null)}
+                />
+              )}
+            </GoogleMap>
+          </div>
+        </LoadScript>
       </div>
     </section>
   );
